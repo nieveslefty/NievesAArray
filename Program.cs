d19b32de82b9413876d10df8ad25c30f600f6918
Alexandra Nieves <alexandranie@usf.edu>
2021-02-05 18:21:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+ Name: Alexandra Nieves
+ Date: 2/5/2021
+ Array assignment
+*/
+
+using System;
+
+namespace Nieves_A_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[25];
+            for(int i = 0; i<myArray.Length; i++)
+                Console.WriteLine("Element value = " + (i+1).ToString());
+            Console.ReadKey();
+        }
+    }
+}
